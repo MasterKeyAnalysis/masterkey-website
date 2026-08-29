b123eb6aef9efd7a2be41a2d9109c16831053447
MasterKeyAnalysis <masterkeyanalysis@zohomail.in>
2026-08-29 20:59:27 +0530

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -807,7 +807,7 @@ async def finance_sample():
 
 # --- APPLICATON ASSEMBLY & MIDDLEWARE ---
 
-app.include_router(api_router, prefix="/api")
+app.include_router(api_router)
 
 app.add_middleware(
     CORSMiddleware,
