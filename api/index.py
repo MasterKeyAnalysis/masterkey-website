5e7e7d5b095c81c495a3c2374e342548b12a20a8
MasterKeyAnalysis <masterkeyanalysis@zohomail.in>
2026-08-31 17:20:43 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,1 +1,7 @@
+import sys
+import os
+
+# Add root directory to sys.path so backend import works
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from backend.server import app
